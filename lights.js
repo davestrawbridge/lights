e9fbd41017a912b2677763985a84f163b8fbf944
David Strawbridge <davidstrawbridge@golfstrike.co.uk>
2015-11-28 22:26:30 +0000

    simplified; bigger lights

--- lights.js
+++ lights.js
@@ -2,19 +2,31 @@ var app = angular.module('app', []);
 
 app.controller('lights', ['$scope', '$interval', function ($scope, $interval) {
     
-    var cancel;
+    var timer = null;
     $scope.isOn = 0;
     $scope.radius = 50;
     
     $scope.start = function() {
-        cancel = $interval(function() {
+        
+        if (timer == null) {
+            timer = $interval(function() {
 
-            if ($scope.isOn < 4)
-                $scope.isOn = $scope.isOn + 1;
-            else
-                $scope.isOn = 1;
-                
-        }, 1000);
+                if ($scope.isOn < 4)
+                    $scope.isOn = $scope.isOn + 1;
+                else
+                    $scope.isOn = 1;
+
+            }, 1000);
+        }
+    }
+    
+    $scope.stop = function() {
+        if (timer != null)
+        {
+            $interval.cancel(timer);
+            $scope.isOn = 0;
+            timer = null;
+        }
     }
 
 }]); 
